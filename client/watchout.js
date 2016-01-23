50fc06815e130dbbb2b8c4a0b5e868fbdb312fa4
ZanderSparrow <sondra.silverhawk@hush.com>
2016-01-22 18:06:18 -0800

    Add leveling up feature

--- client/watchout.js
+++ client/watchout.js
@@ -1,16 +1,25 @@
 // start slingin' some d3 here.
 
-  var width = $(window).width(),
-    height = $(window).height();
+  var width = $(window).width();
+  var height = $(window).height();
 
   var asteroids = [];
   var minAsteroids = 5;
   var maxAsteroids = 20;
-  var numAsteroids = Math.floor(Math.random() * (maxAsteroids - minAsteroids) + minAsteroids);
   var fps = 100;
   var svg;
   var collisions = 0;
+  var score = 0;
+  var highScore = 0;
+  var level = 1;
 
+
+var levelInit = function(level) {
+
+  asteroids = [];
+  window.level = level;
+  $('.level span').html(level);
+  var numAsteroids = Math.floor(Math.random() * (maxAsteroids - minAsteroids) + minAsteroids * level);
   for (var i = 0; i < numAsteroids; i++) {
     var dimension = Math.floor(Math.random() * 80 + 20);
     var xPos = Math.floor(Math.random() * (width - dimension));
@@ -35,6 +44,49 @@
       vy : Math.floor(vel * Math.sin(angle))
     });
   }
+  populateAsteroids();
+};
+
+
+var populateAsteroids = function() {
+  // Data Join
+  var borders = svg.selectAll('.border')
+    .data(asteroids);
+
+  var images = svg.selectAll('.asteroid')
+    .data(asteroids);
+
+
+
+// UPDATE
+
+
+// ENTER
+    borders.enter()
+    .append("circle")
+    .attr("class", 'border')
+    .attr("cx", function(d) { return d.x + d.width / 2; })
+    .attr("cy", function(d) { return d.y + d.height / 2; })
+    .attr("r", function(d) { return d.width * 0.6; });
+
+
+    images.enter()
+    .append('svg:image')
+    .attr("xlink:href","asteroid.png")
+    .attr('x', function(d) { return d.x; })
+    .attr('y', function(d) { return d.y; })
+    .attr('width', function(d) { return d.width; })
+    .attr('height', function(d) { return d.height; })
+    .attr('class', 'asteroid');
+
+// EXIT
+    borders.exit().remove();
+    images.exit().remove();
+
+
+};
+
+
 
   var hero = {
     x:0,
@@ -45,7 +97,11 @@
 
   var updatePos = function() {
 
-
+    $('.current span').html(score++);
+    if(score > highScore) {
+      highScore = score;
+      $('.highscore span').html(highScore);
+    }
     asteroids.forEach(function(asteroid) {
       if ((asteroid.x) < 0 || (asteroid.x + asteroid.width) > width) {
         asteroid.vx *= -1;
@@ -56,10 +112,14 @@
       asteroid.x += asteroid.vx;
       asteroid.y += asteroid.vy;
 
-      if (radialCollision(hero, asteroid)) {
+      if (radialCollision(hero, asteroid) && !asteroid.collision) {
         $('.collisions span').html(collisions++);
         hero.collision = true;
         asteroid.collision = true;
+        score = 0;
+        $('.current span').html(score);
+            levelInit(1);
+            
 
       } else {
         hero.collision = false;
@@ -82,7 +142,16 @@
       .attr("cx", hero.x + hero.width / 2 )
       .attr("cy", hero.y + hero.height / 2)
       .classed('.collision', hero.collision);
-  };
+
+
+  if ( score > 0 && score % 1000 === 0) {
+    levelInit(window.level + 1);
+  }
+
+};
+
+
+
 
 var hasCollision = function(rect1, rect2)  {
  return rect1.x < rect2.x + rect2.width &&
@@ -101,29 +170,14 @@ return (distance < rect1.width / 2 + rect2.width / 2);
 
 $( document ).ready( function() {
 
+
+  
   svg = d3.select( ".board" ).append( "svg" )
     .attr( "width", width )
     .attr( "height", height );
 
-  svg.selectAll('.border')
-    .data(asteroids)
-    .enter().
-    append("circle")
-    .attr("class", 'border')
-    .attr("cx", function(d) { return d.x + d.width / 2; })
-    .attr("cy", function(d) { return d.y + d.height / 2; })
-    .attr("r", function(d) { return d.width * 0.6; });
+levelInit(level);
 
-  svg.selectAll('.asteroid')
-    .data(asteroids)
-    .enter()
-    .append('svg:image')
-    .attr("xlink:href","asteroid.png")
-    .attr('x', function(d) { return d.x; })
-    .attr('y', function(d) { return d.y; })
-    .attr('width', function(d) { return d.width; })
-    .attr('height', function(d) { return d.height; })
-    .attr('class', 'asteroid');
 
   svg.append('svg:image')
     .attr("xlink:href","kodos.png")
@@ -131,19 +185,13 @@ $( document ).ready( function() {
     .attr('width', 50)
     .attr('height', 100);
 
-    svg.selectAll('.hero-border')
-    .append("circle")
-    .attr("class", 'hero-border')
-    .attr("cx", function(d) { return hero.x + hero.width / 2; })
-    .attr("cy", function(d) { return hero.y + hero.height / 2; })
-    .attr("r", function(d) { return hero.height * 0.6; });
-
   svg.on("mousemove", function() { 
     var mousePos = d3.mouse(this); 
     svg.select('.hero')
       .attr('x', hero.x = mousePos[0])
       .attr('y', hero.y = mousePos[1]); 
-  });
+  }); 
+
 
 
   setInterval(function() {
