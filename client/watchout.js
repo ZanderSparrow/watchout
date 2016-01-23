9cbdd9762e0b939155ab9acdba88275aafc48c1e
ZanderSparrow <sondra.silverhawk@hush.com>
2016-01-22 16:35:18 -0800

    Red circle behind collided asteroids, radial collision detection instead of rectangular

--- client/watchout.js
+++ client/watchout.js
@@ -1,20 +1,20 @@
 // start slingin' some d3 here.
 
-  var width = 800,
-    height = 600;
+  var width = $(window).width(),
+    height = $(window).height();
 
   var asteroids = [];
   var minAsteroids = 5;
   var maxAsteroids = 20;
   var numAsteroids = Math.floor(Math.random() * (maxAsteroids - minAsteroids) + minAsteroids);
-  var fps = 60;
+  var fps = 100;
   var svg;
   var collisions = 0;
 
   for (var i = 0; i < numAsteroids; i++) {
     var dimension = Math.floor(Math.random() * 80 + 20);
-    var xPos = Math.floor(Math.random() * width - dimension);
-    var yPos = Math.floor(Math.random() * height - dimension);
+    var xPos = Math.floor(Math.random() * (width - dimension));
+    var yPos = Math.floor(Math.random() * (height - dimension));
     
     // avoid corner case where asteroid starts out of bounds
     if (xPos + dimension > width) { 
@@ -31,8 +31,8 @@
       y: yPos,
       width: dimension,
       height: dimension,
-      vx : vel * Math.cos(angle),
-      vy : vel * Math.sin(angle)
+      vx : Math.floor(vel * Math.cos(angle)),
+      vy : Math.floor(vel * Math.sin(angle))
     });
   }
 
@@ -56,19 +56,30 @@
       asteroid.x += asteroid.vx;
       asteroid.y += asteroid.vy;
 
-      if (hasCollision(hero, asteroid)) {
+      if (radialCollision(hero, asteroid)) {
         $('.collisions span').html(collisions++);
+        hero.collision = true;
+        asteroid.collision = true;
+
+      } else {
+        hero.collision = false;
+        asteroid.collision = false;
       }
-
-
     });
 
 
     svg.selectAll(".asteroid")
       .attr('x', function(d) { return d.x; })
-      .attr('y', function(d) { return d.y; })
-      .classed('collision', function(d) { return d.collision; });
+      .attr('y', function(d) { return d.y; });
     
+    svg.selectAll(".border")
+    .attr("cx", function(d) { return d.x + d.width / 2; })
+    .attr("cy", function(d) { return d.y + d.height / 2; })
+      .classed('collision', function(d) { return d.collision; });
+
+
+    svg.select('hero-border')
+      .classed('.collision', hero.collision);
   };
 
 var hasCollision = function(rect1, rect2)  {
@@ -78,11 +89,28 @@ var hasCollision = function(rect1, rect2)  {
    rect1.height + rect1.y > rect2.y;
 };
 
+var radialCollision = function(rect1, rect2) {
+  var dx = (rect1.x + rect1.width/2) - (rect2.x + rect2.width/2);
+  var dy = (rect1.y + rect1.height/2) - (rect2.y + rect2.height/2);
+  var distance = Math.sqrt(dx * dx + dy * dy);
+
+return (distance < rect1.width / 2 + rect2.width / 2);
+};
+
 $( document ).ready( function() {
 
   svg = d3.select( ".board" ).append( "svg" )
     .attr( "width", width )
     .attr( "height", height );
+
+  svg.selectAll('.border')
+    .data(asteroids)
+    .enter().
+    append("circle")
+    .attr("class", 'border')
+    .attr("cx", function(d) { return d.x + d.width / 2; })
+    .attr("cy", function(d) { return d.y + d.height / 2; })
+    .attr("r", function(d) { return d.width * 0.6; });
 
   svg.selectAll('.asteroid')
     .data(asteroids)
@@ -101,6 +129,13 @@ $( document ).ready( function() {
     .attr('width', 50)
     .attr('height', 100);
 
+    svg.selectAll('.hero-border')
+    .append("circle")
+    .attr("class", 'hero-border')
+    .attr("cx", function(d) { return hero.x + hero.width / 2; })
+    .attr("cy", function(d) { return hero.y + hero.height / 2; })
+    .attr("r", function(d) { return hero.height * 0.6; });
+
   svg.on("mousemove", function() { 
     var mousePos = d3.mouse(this); 
     svg.select('.hero')
@@ -111,6 +146,6 @@ $( document ).ready( function() {
 
   setInterval(function() {
     updatePos();
-  }, 1000 / fps);
+  }, Math.floor(1000 / fps));
 
 });  // document ready end
