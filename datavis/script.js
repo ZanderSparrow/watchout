bdc3dec4481239a652624281a401187a9431b030
ZanderSparrow <sondra.silverhawk@hush.com>
2016-01-23 16:48:39 -0800

    Tooltips to show health spending per state

--- datavis/script.js
+++ datavis/script.js
@@ -1,29 +1,23 @@
 var state_abbr = {"ALABAMA":"AL","ALASKA":"AK","AMERICAN SAMOA":"AS","ARIZONA":"AZ","ARKANSAS":"AR","CALIFORNIA":"CA","COLORADO":"CO","CONNECTICUT":"CT","DELAWARE":"DE","DISTRICT OF COLUMBIA":"DC","FEDERATED STATES OF MICRONESIA":"FM","FLORIDA":"FL","GEORGIA":"GA","GUAM":"GU","HAWAII":"HI","IDAHO":"ID","ILLINOIS":"IL","INDIANA":"IN","IOWA":"IA","KANSAS":"KS","KENTUCKY":"KY","LOUISIANA":"LA","MAINE":"ME","MARSHALL ISLANDS":"MH","MARYLAND":"MD","MASSACHUSETTS":"MA","MICHIGAN":"MI","MINNESOTA":"MN","MISSISSIPPI":"MS","MISSOURI":"MO","MONTANA":"MT","NEBRASKA":"NE","NEVADA":"NV","NEW HAMPSHIRE":"NH","NEW JERSEY":"NJ","NEW MEXICO":"NM","NEW YORK":"NY","NORTH CAROLINA":"NC","NORTH DAKOTA":"ND","NORTHERN MARIANA ISLANDS":"MP","OHIO":"OH","OKLAHOMA":"OK","OREGON":"OR","PALAU":"PW","PENNSYLVANIA":"PA","PUERTO RICO":"PR","RHODE ISLAND":"RI","SOUTH CAROLINA":"SC","SOUTH DAKOTA":"SD","TENNESSEE":"TN","TEXAS":"TX","UTAH":"UT","VERMONT":"VT","VIRGIN ISLANDS":"VI","VIRGINIA":"VA","WASHINGTON":"WA","WEST VIRGINIA":"WV","WISCONSIN":"WI","WYOMING":"WY"};
 
 $(function(){
-  var dataset;
-  //var keyfn = function(k){ return k.abbr; };
+  var dataset = {};
 
-  // for(var state in state_abbr) {
-  //   var nonsense = 0;
-  //   var obj = {
-  //     state: state,
-  //     abbr: state_abbr[state],
-  //     nonsense: nonsense
-  //   };
-  //   dataset.push(obj);
-  //   nonsense++;
-  // }
-  // d3.csv('US_PER_CAPITA09.CSV', function(data) {
-  //   window.dataset = data.map(function(d) {
-  //     if(d['State_Name']) {
-  //       console.log(d['State_Name']);
-  //       return {state: d['State_Name'], abbr: state_abbr[d['State_Name'].toUpperCase()],spending: +d['Y2009'] }; 
-  //     } 
-  //   });
-  // });
+  var toolTip = d3.select('body').append('div')
+  .attr('class', 'tooltip')
+  .style('opacity', 0);
 
-$.ajax({
+  function getCentroid(selection) {
+    // get the DOM element from a D3 selection
+    // you could also use "this" inside .each()
+    var element = selection.node(),
+        // use the native SVG interface to get the bounding box
+        bbox = element.getBBox();
+    // return the center of the bounding box
+    return [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
+  }
+
+  $.ajax({
     type: "GET",
     url: "US_PER_CAPITA09.CSV",
     dataType: "text",
@@ -31,31 +25,64 @@ $.ajax({
             dat3 = d3.csv.parse(data);
             procesData(dat3);
           }
-});
+  });
 
-var procesData = function(data) {
-  
-  dataset = data.map(function(d) {
+var getMin = function(obj, key) {
+  var min = Infinity;
+  for(var item in obj) {
+    if(obj[item][key] < min) {
+      min = obj[item][key];
+    }
+  }
+  return min;
+};
+
+var getMax = function(obj, key) {
+  var max = -Infinity;
+  for(var item in obj) {
+    if(obj[item][key] > max) {
+      max = obj[item][key];
+    }
+  }
+  return max;
+};
+
+  var procesData = function(data) {
+
+    data.forEach(function(d) {
       if(d['State_Name']) {
-        console.log(d['State_Name']);
-        return {state: d['State_Name'], abbr: state_abbr[d['State_Name'].toUpperCase()],spending: +d['Y2009'] }; 
+        var abbr = state_abbr[d['State_Name'].toUpperCase()];
+        dataset[abbr] = {state: d['State_Name'], spending: +d['Y2009'] }; 
       } 
     });
 
-  // Bind the data by state names
-  var states = d3.selectAll('path')
-    .data(dataset);
-
-  states.attr('fill', function(d){
- 
-    // Get the ID of the path we are currently working with
-    // Our SVG uses the state abbreviation for the ID
-    var abbr = this.id;
-   
-    // Return colors
-    // based on data          
     
+    var colorScale = d3.scale.linear()
+      .domain([getMin(dataset, 'spending'), getMax(dataset, 'spending')])
+      .range(["white", "black"]);
 
-  });
-};
+    var states = d3.selectAll('.state');
+
+    states.attr('fill', function(d){
+      var stateData = dataset[this.id];
+      return colorScale(stateData.spending);
+    })
+    .on("mouseover", function(d) {
+      var stateData = dataset[this.id];
+      toolTip.transition()
+        .duration(300)
+        .style("opacity", 0.9);
+      toolTip.html(stateData.state + ": $" + stateData.spending)
+        .style("left", function() {
+          // var centroid = getCentroid(this);
+          return(d3.event.pageX) + "px";
+        })
+        .style("top", (d3.event.pageY - 28) + "px")
+      .on("mouseout", function(d) {
+        toolTip.transition()
+          .duration(500)
+          .style("opacity", 0);
+      });
+    });
+  };
 });
